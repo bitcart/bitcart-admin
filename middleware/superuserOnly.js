53c414d2480c8d6673ed04769afcc3e58346e873
MrNaif2018 <chuff184@gmail.com>
2019-12-07 23:50:57 +0300

    Fix superuser middleware

--- middleware/superuserOnly.js
+++ middleware/superuserOnly.js
@@ -1,5 +1,5 @@
 export default function ({ store, redirect }) {
-  if (!store.state.auth.user.is_superuser) {
+  if (!store.state.auth.user || !store.state.auth.user.is_superuser) {
     return redirect('/')
   }
 }
