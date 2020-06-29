a46312ac8ee0ef012efe2ae53d8160e95f580642
MrNaif2018 <chuff184@gmail.com>
2020-06-29 21:46:45 +0300

    Fix mdi-upload icon on production

--- plugins/imageinput.js
+++ plugins/imageinput.js
@@ -1,4 +1,9 @@
-import Vue from 'vue'
-import VImageInput from 'vuetify-image-input'
+const VImageInput = () => import(process.env.NODE_ENV === 'production' ? 'vuetify-image-input/a-la-carte' : 'vuetify-image-input').then(m => m.default)
+const VueLib = () => import('vue').then(m => m.default)
 
-Vue.component('VImageInput', VImageInput)
+async function start () {
+  const Vue = await VueLib()
+  Vue.component('VImageInput', VImageInput)
+}
+
+start()
