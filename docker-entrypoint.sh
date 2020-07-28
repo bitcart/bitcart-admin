4e51cf7585b9c330e0ae2dcbfb95e234b10d6d42
MrNaif2018 <chuff184@gmail.com>
2020-07-28 13:11:45 +0300

    Export env variable in docker entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,5 +2,6 @@
 SERVICE_FILE=/var/lib/tor/hidden_services/BitcartCC-Merchants-API/hostname
 if [ -f $SERVICE_FILE ]; then
     read -r BITCART_ADMIN_ONION_URL < $SERVICE_FILE
+    export BITCART_ADMIN_ONION_URL="$BITCART_ADMIN_ONION_URL"
 fi
 yarn start
