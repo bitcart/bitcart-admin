fd56336003f75740da7cf2010d559b0998140af5
MrNaif2018 <chuff184@gmail.com>
2020-07-28 13:55:57 +0300

    Fix onion url exporting

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,6 +2,6 @@
 SERVICE_FILE=/var/lib/tor/hidden_services/BitcartCC-Merchants-API/hostname
 if [ -f $SERVICE_FILE ]; then
     read -r BITCART_ADMIN_ONION_URL < $SERVICE_FILE
-    export BITCART_ADMIN_ONION_URL="$BITCART_ADMIN_ONION_URL"
+    export BITCART_ADMIN_ONION_URL="http://$BITCART_ADMIN_ONION_URL"
 fi
 yarn start
