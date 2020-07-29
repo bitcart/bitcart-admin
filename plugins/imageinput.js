3ff00794230699300979265e07f22d7debc9d79a
MrNaif2018 <chuff184@gmail.com>
2020-07-29 19:14:49 +0300

    Fixed possible memory leak

--- plugins/imageinput.js
+++ plugins/imageinput.js
@@ -1,9 +1,9 @@
-const VImageInput = () => import(process.env.NODE_ENV === 'production' ? 'vuetify-image-input/a-la-carte' : 'vuetify-image-input').then(m => m.default)
-const VueLib = () => import('vue').then(m => m.default)
-
-async function start () {
-  const Vue = await VueLib()
-  Vue.component('VImageInput', VImageInput)
+import Vue from 'vue'
+let VImageInput
+if (process.env.NODE_ENV === 'production') {
+  VImageInput = require('vuetify-image-input/a-la-carte').default
+} else {
+  VImageInput = require('vuetify-image-input')
 }
 
-start()
+Vue.component('v-image-input', VImageInput)
