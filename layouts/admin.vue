84b52b900dd6ac86e4f205a120f43bbe6f0035fd
MrNaif2018 <chuff184@gmail.com>
2020-10-23 22:08:37 +0300

    Fix responsive issues

--- layouts/admin.vue
+++ layouts/admin.vue
@@ -1,12 +1,23 @@
 <template>
   <Dashboard>
-    <v-toolbar class="mb-3">
+    <v-toolbar class="mb-3 hidden-sm-and-down">
       <v-toolbar-items>
         <v-btn v-for="item in items" :key="item.text" :to="item.to" exact text>
           {{ item.text }}
         </v-btn>
       </v-toolbar-items>
     </v-toolbar>
+    <div class="mb-3 hidden-md-and-up">
+      <v-divider />
+      <v-list>
+        <v-list-item v-for="item in items" :key="item.text" :to="item.to" exact>
+          <v-list-item-content>
+            <v-list-item-title v-text="item.text" />
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+      <v-divider />
+    </div>
   </Dashboard>
 </template>
 <script>
