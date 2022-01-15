8bbe0f561c3647b85cb9d6d1aebb1c43d75d8839
MrNaif2018 <chuff184@gmail.com>
2022-01-16 01:20:45 +0300

    Upgrade all packages

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,8 @@ module.exports = {
     node: true,
   },
   parserOptions: {
-    parser: "babel-eslint",
+    parser: "@babel/eslint-parser",
+    requireConfigFile: false,
   },
   extends: [
     "@nuxtjs",
@@ -27,5 +28,6 @@ module.exports = {
         allowModifiers: true,
       },
     ],
+    "vue/multi-word-component-names": "off",
   },
 }
