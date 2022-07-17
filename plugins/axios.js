3a85f8bd2db94f6433c7ff61c6a69f8c88477876
MrNaif2018 <chuff184@gmail.com>
2022-07-17 22:00:36 +0300

    Fix socks agent usage

--- plugins/axios.js
+++ plugins/axios.js
@@ -1,7 +1,7 @@
 export default async ({ store, $axios, $config, req }) => {
   await store.dispatch("loadEnv", { env: $config, req })
   if (process.server && store.state.onion && $config.SOCKS_PROXY) {
-    const SocksProxyAgent = require("socks-proxy-agent")
+    const { SocksProxyAgent } = require("socks-proxy-agent")
     const agent = new SocksProxyAgent($config.SOCKS_PROXY)
     $axios.defaults.httpsAgent = agent
     $axios.defaults.httpAgent = agent
