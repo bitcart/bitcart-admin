c08fd46515cbe88c3b6fd65e635a0833a9c01851
MrNaif2018 <chuff184@gmail.com>
2022-08-12 14:12:43 +0300

    Added sign & broadcast dialog

--- pages/payouts.vue
+++ pages/payouts.vue
@@ -6,25 +6,80 @@
     :url="url"
     :title="title"
     :custom-batch-actions="batchActions"
+    :processbatch="processbatch"
   >
     <template #before-toolbar>
       <search-filters :search.sync="search" :custom-filters="customFilters" />
+      <v-dialog v-model="showSendDialog" max-width="700px">
+        <v-card>
+          <v-card-title
+            >Sign payouts
+            <v-btn
+              icon
+              target="_blank"
+              href="https://docs.bitcartcc.com/guides/payouts"
+            >
+              <v-icon medium> mdi-help-circle-outline </v-icon>
+            </v-btn></v-card-title
+          >
+          <v-card-text>
+            <v-container>
+              <v-row v-for="(wallet, index) in wallets" :key="index">
+                <v-col cols="3">
+                  <auto-complete
+                    v-model="wallets[index][0]"
+                    url="wallets"
+                    label="Wallet"
+                  />
+                </v-col>
+                <v-col cols="8">
+                  <v-text-field
+                    v-model="wallets[index][1]"
+                    label="Private key"
+                  />
+                </v-col>
+                <v-col cols="1" class="mt-4">
+                  <v-btn icon @click="wallets.splice(index, 1)">
+                    <tooltip-icon icon="close" text="Delete" />
+                  </v-btn>
+                </v-col>
+              </v-row>
+              <v-row>
+                <v-btn color="primary" @click="wallets.push(['', ''])"
+                  >Add private key</v-btn
+                >
+              </v-row>
+            </v-container>
+          </v-card-text>
+          <v-card-actions class="justify-center pb-5">
+            <v-btn color="primary" @click="sendBatchCommand"
+              >Sign & broadcast</v-btn
+            >
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
     </template>
   </item-data>
 </template>
 <script>
 import ItemData from "@/components/ItemData.vue"
 import SearchFilters from "@/components/SearchFilters.vue"
+import AutoComplete from "@/components/AutoComplete.vue"
+import TooltipIcon from "@/components/TooltipIcon"
 
 export default {
   components: {
     ItemData,
     SearchFilters,
+    AutoComplete,
+    TooltipIcon,
   },
   layout: "dashboard",
   data() {
     return {
       search: "",
+      showSendDialog: false,
+      wallets: [],
       headers: [
         { text: "ID", value: "id", mode: "display", copy: true },
         {
@@ -116,6 +171,23 @@ export default {
         },
       ],
     }
+  },
+  methods: {
+    processbatch(command) {
+      if (command === "send") {
+        if (!this.showSendDialog) {
+          this.wallets = []
+          this.showSendDialog = true
+        } else {
+          this.showSendDialog = false
+          return { options: { wallets: Object.fromEntries(this.wallets) } }
+        }
+        return null
+      } else return {}
+    },
+    sendBatchCommand() {
+      this.$refs.itemdata.processBatchCommand("send", this.wallets)
+    },
   },
 }
 </script>
