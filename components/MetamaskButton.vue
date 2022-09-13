ec15276ef53de9b1b92c5390289a6581f8bf2557
MrNaif2018 <chuff184@gmail.com>
2022-09-13 19:58:21 +0300

    Refactor

--- components/MetamaskButton.vue
+++ components/MetamaskButton.vue
@@ -72,96 +72,16 @@ export default {
       this.showMessage(false, text)
     },
     async connectToMetamask() {
-      this.loading = true
       if (window.ethereum) {
         this.web3 = new window.Web3(window.ethereum)
-        const chainid = await this.web3.eth.getChainId()
-        if (chainid !== this.method.chain_id)
-          return this.showError(
-            `Please change your network in metamask to work on the ${this.method.currency.toUpperCase()} network (chain id ${
-              this.method.chain_id
-            })`
-          )
-        try {
-          await window.ethereum.request({ method: "eth_requestAccounts" })
-        } catch (error) {
-          return this.showError(error.message)
-        }
-        let balance = 0
-        let divisibility = 18 // for all EVM-based blockchains
-        try {
-          if (!this.method.contract)
-            balance = this.web3.utils.toBN(
-              await this.web3.eth.getBalance(window.ethereum.selectedAddress)
-            )
-          else {
-            const contract = new this.web3.eth.Contract(
-              this.abi[this.method.currency],
-              this.method.contract
-            )
-            balance = this.web3.utils.toBN(
-              await contract.methods
-                .balanceOf(window.ethereum.selectedAddress)
-                .call()
-            )
-            divisibility = await contract.methods.decimals().call()
-          }
-        } catch (error) {
-          return this.showError(error.message)
-        }
-        const requiredAmount = this.web3.utils.toBN(
-          this.toWei(this.method.amount, divisibility)
+        await this.$utils.connectToWallet.call(
+          this,
+          "metamask",
+          () => window.ethereum.selectedAddress
         )
-        this.loading = false
-        if (balance.lt(requiredAmount)) this.insufficientBalance = true
-        else {
-          this.showMessage(true, "Connected to metamask!")
-          await this.payWithMetamask()
-        }
       } else {
         window.location.href = "https://metamask.io/download"
       }
-    },
-    async payWithMetamask() {
-      this.loading = true
-      let divisibility = 18
-      let contract = null
-      if (this.method.contract) {
-        contract = new this.web3.eth.Contract(
-          this.abi[this.method.currency],
-          this.method.contract
-        )
-        divisibility = await contract.methods.decimals().call()
-      }
-      const requiredAmount = this.toWei(this.method.amount, divisibility)
-      try {
-        if (!this.method.contract) {
-          // usual tx
-          await this.web3.eth.sendTransaction({
-            to: this.method.payment_address,
-            from: window.ethereum.selectedAddress,
-            value: requiredAmount,
-          })
-        } else {
-          // contract payment
-          await contract.methods
-            .transfer(this.method.payment_address, requiredAmount)
-            .send({ from: window.ethereum.selectedAddress })
-        }
-      } catch (error) {
-        this.showError(error.message)
-      }
-    },
-    toWei(ether, unit) {
-      const base = new this.web3.utils.BN(10).pow(new this.web3.utils.BN(unit))
-      let [whole, fraction] = ether.split(".")
-      if (!whole) whole = "0"
-      if (!fraction) fraction = "0"
-      while (fraction.length < unit) fraction += "0"
-      whole = new this.web3.utils.BN(whole)
-      fraction = new this.web3.utils.BN(fraction)
-      const wei = whole.mul(base).add(fraction)
-      return wei.toString(10)
     },
   },
 }
--- components/WalletConnectButton.vue
+++ components/WalletConnectButton.vue
@@ -4,7 +4,7 @@
       color="primary"
       :disabled="insufficientBalance"
       :loading="loading"
-      @click="connectToMetamask"
+      @click="connect"
     >
       <template v-if="insufficientBalance">Insuficient Balance</template>
       <template v-else>
@@ -76,7 +76,7 @@ export default {
     showError(text) {
       this.showMessage(false, text)
     },
-    async connectToMetamask() {
+    async connect() {
       // eslint-disable-next-line
       const provider = new window.WalletConnectProvider.default({
         infuraId: "13ddd02141f04d93990f7d016217f06e", // Required
@@ -85,87 +85,20 @@ export default {
         this.address = accounts[0]
       })
       //  Enable session (triggers QR Code modal)
-      await provider.enable()
-      //  Create Web3
-      this.web3 = new window.Web3(provider)
-      this.loading = true
-      const chainid = await this.web3.eth.getChainId()
-      if (chainid !== this.method.chain_id)
-        return this.showError(
-          `Please change your network in metamask to work on the ${this.method.currency.toUpperCase()} network (chain id ${
-            this.method.chain_id
-          })`
-        )
-      let balance = 0
-      let divisibility = 18 // for all EVM-based blockchains
+      window?.localStorage?.removeItem("walletconnect")
       try {
-        if (!this.method.contract)
-          balance = this.web3.utils.toBN(
-            await this.web3.eth.getBalance(this.address)
-          )
-        else {
-          const contract = new this.web3.eth.Contract(
-            this.abi[this.method.currency],
-            this.method.contract
-          )
-          balance = this.web3.utils.toBN(
-            await contract.methods.balanceOf(this.address).call()
-          )
-          divisibility = await contract.methods.decimals().call()
-        }
+        await provider.enable()
       } catch (error) {
         return this.showError(error.message)
       }
-      const requiredAmount = this.web3.utils.toBN(
-        this.toWei(this.method.amount, divisibility)
+      //  Create Web3
+      this.web3 = new window.Web3(provider)
+
+      await this.$utils.connectToWallet.call(
+        this,
+        "walletconnect",
+        () => this.address
       )
-      this.loading = false
-      if (balance.lt(requiredAmount)) this.insufficientBalance = true
-      else {
-        this.showMessage(true, "Connected to metamask!")
-        await this.payWithMetamask()
-      }
-    },
-    async payWithMetamask() {
-      this.loading = true
-      let divisibility = 18
-      let contract = null
-      if (this.method.contract) {
-        contract = new this.web3.eth.Contract(
-          this.abi[this.method.currency],
-          this.method.contract
-        )
-        divisibility = await contract.methods.decimals().call()
-      }
-      const requiredAmount = this.toWei(this.method.amount, divisibility)
-      try {
-        if (!this.method.contract) {
-          // usual tx
-          await this.web3.eth.sendTransaction({
-            to: this.method.payment_address,
-            from: this.address,
-            value: requiredAmount,
-          })
-        } else {
-          // contract payment
-          await contract.methods
-            .transfer(this.method.payment_address, requiredAmount)
-            .send({ from: this.address })
-        }
-      } catch (error) {
-        this.showError(error.message)
-      }
-    },
-    toWei(ether, unit) {
-      const base = new this.web3.utils.BN(10).pow(new this.web3.utils.BN(unit))
-      let [whole, fraction] = ether.split(".")
-      if (!whole) whole = "0"
-      if (!fraction) fraction = "0"
-      while (fraction.length < unit) fraction += "0"
-      whole = new this.web3.utils.BN(whole)
-      fraction = new this.web3.utils.BN(fraction)
-      const wei = whole.mul(base).add(fraction)
-      return wei.toString(10)
     },
   },
 }
