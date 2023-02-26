e1b2ac1d668aedcc4367b5e19f48f261b886b3ec
MrNaif2018 <chuff184@gmail.com>
2023-02-27 01:30:27 +0300

    Fix merging settings for plugins

--- store/dictionaries/state.js
+++ store/dictionaries/state.js
@@ -1,10 +1,22 @@
+import mergeWith from "lodash.mergewith"
 import extendsModules from "~/.nuxt/extends.modules"
+
+function customizer(objValue, srcValue) {
+  if (Array.isArray(objValue)) {
+    return objValue.concat(srcValue)
+  }
+}
 
 const dictionaries = Object.values(extendsModules).reduce((acc, current) => {
   let connectedDict = acc
 
   if (current.dictionaries) {
-    connectedDict = Object.assign({}, connectedDict, current.dictionaries)
+    connectedDict = mergeWith(
+      {},
+      connectedDict,
+      current.dictionaries,
+      customizer
+    )
   }
   return connectedDict
 }, [])
