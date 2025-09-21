8bde02f23c83a2b566d19c22641d8695164be85e
MrNaif2018 <chuff184@gmail.com>
2025-09-21 23:59:52 +0300

    Update version number for clarity

--- version.js
+++ version.js
@@ -1,4 +1,4 @@
-export const VERSION = "0.9.1.0"
+export const VERSION = "0.10.0.0"
 export const LICENSING_SERVER = "https://licensing.bitcart.ai"
 export const LICENSING_CHECKOUT_URL = "https://admin.bitcart.ai"
 
