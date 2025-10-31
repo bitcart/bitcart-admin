ee1e55869f3f1a1102bcd8fb01cb02f3b74ca400
MrNaif2018 <chuff184@gmail.com>
2025-10-31 18:22:05 +0300

    Version 0.10.1.0

--- version.js
+++ version.js
@@ -1,4 +1,4 @@
-export const VERSION = "0.10.0.1"
+export const VERSION = "0.10.1.0"
 export const LICENSING_SERVER = "https://licensing.bitcart.ai"
 export const LICENSING_CHECKOUT_URL = "https://admin.bitcart.ai"
 
