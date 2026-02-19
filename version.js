d0e946aeabda60c140640c8ebf29be879a548381
MrNaif2018 <chuff184@gmail.com>
2026-02-20 00:08:43 +0300

    Version 0.10.2.0

--- version.js
+++ version.js
@@ -1,4 +1,4 @@
-export const VERSION = "0.10.1.1"
+export const VERSION = "0.10.2.0"
 export const LICENSING_SERVER = "https://licensing.bitcart.ai"
 export const LICENSING_CHECKOUT_URL = "https://admin.bitcart.ai"
 export const ETH_PLUGIN_ID = "ZIDVfTVxDWQLKyhYiqifyNkhcvfufUKm"
