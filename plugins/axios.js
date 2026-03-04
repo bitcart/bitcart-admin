3bd9989c58a322d114e591619a96ab86fa8feaf1
MrNaif2018 <chuff184@gmail.com>
2026-03-04 13:38:42 +0300

    Fix onion serverside when container shortcut is active

--- plugins/axios.js
+++ plugins/axios.js
@@ -1,6 +1,11 @@
 export default async ({ store, $axios, $config, req }) => {
   await store.dispatch("loadEnv", { env: $config, req })
-  if (process.server && store.state.onion && $config.SOCKS_PROXY) {
+  if (
+    process.server &&
+    store.state.onion &&
+    $config.SOCKS_PROXY &&
+    !store.state.env.SERVER_API_URL
+  ) {
     const { SocksProxyAgent } = require("socks-proxy-agent")
     const agent = new SocksProxyAgent($config.SOCKS_PROXY)
     $axios.defaults.httpsAgent = agent
